0d52cf24ae7b4ff47afe628626beeebf769147c8
10664kls <phuangpeht.phouminith@krungsri.com>
2025-06-04 16:57:46 +0700

    To add function saveWordlist

--- internal/income/wordlist.go
+++ internal/income/wordlist.go
@@ -8,8 +8,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/10664kls/automatic-finance-api/internal/database"
 	"github.com/10664kls/automatic-finance-api/internal/pager"
 	sq "github.com/Masterminds/squirrel"
+	edPb "google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	rpcStatus "google.golang.org/grpc/status"
 )
 
 // ErrWordlistNotFound is returned when a wordlist is not found in the database.
@@ -20,7 +24,17 @@ type Wordlist struct {
 	Word      string    `json:"word"`
 	Category  source    `json:"category"`
 	CreatedBy string    `json:"createdBy"`
+	UpdatedBy string    `json:"updatedBy"` // Optional, can be used for updates
 	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+func (w *Wordlist) Update(by string, in *WordlistReq) bool {
+	w.Word = strings.TrimSpace(in.Word)
+	w.Category = in.Category
+	w.UpdatedBy = by
+	w.UpdatedAt = time.Now()
+	return true
 }
 
 type ListWordlistsResult struct {
@@ -72,6 +86,57 @@ func (q *WordlistQuery) ToSql() (string, []any, error) {
 	return and.ToSql()
 }
 
+type WordlistReq struct {
+	// ID is used for updating an existing wordlist.
+	ID int64 `json:"-" param:"id"`
+
+	Word     string `json:"word"`
+	Category source `json:"category"`
+}
+
+func (r *WordlistReq) Validate() error {
+	violations := make([]*edPb.BadRequest_FieldViolation, 0)
+
+	if r.Word == "" {
+		violations = append(violations, &edPb.BadRequest_FieldViolation{
+			Field:       "word",
+			Description: "Word must not be empty",
+		})
+	}
+
+	if r.Category == SourceUnSpecified {
+		violations = append(violations, &edPb.BadRequest_FieldViolation{
+			Field:       "category",
+			Description: "Category must not be empty",
+		})
+	}
+
+	if len(violations) > 0 {
+		s, _ := rpcStatus.New(
+			codes.InvalidArgument,
+			"Transaction is not valid or incomplete. Please check the errors and try again, see details for more information.",
+		).WithDetails(&edPb.BadRequest{
+			FieldViolations: violations,
+		})
+
+		return s.Err()
+	}
+
+	return nil
+}
+
+func (r *WordlistReq) ToWordlist(by string) *Wordlist {
+	return &Wordlist{
+		ID:        r.ID,
+		Word:      strings.TrimSpace(r.Word),
+		Category:  r.Category,
+		CreatedBy: by,
+		CreatedAt: time.Now(),
+		UpdatedBy: by,
+		UpdatedAt: time.Now(),
+	}
+}
+
 func matchWordlists(target string, wordlists []*Wordlist) (source, string, bool) {
 	target = strings.TrimSpace(target)
 	target = strings.ToLower(target)
@@ -102,6 +167,8 @@ func listWordlists(ctx context.Context, db *sql.DB, in *WordlistQuery) ([]*Wordl
 			"category",
 			"created_by",
 			"created_at",
+			"updated_by",
+			"updated_at",
 		).
 		From(`income_wordlist`).
 		Where(pred, args...).
@@ -125,6 +192,8 @@ func listWordlists(ctx context.Context, db *sql.DB, in *WordlistQuery) ([]*Wordl
 			&wordlist.Category,
 			&wordlist.CreatedBy,
 			&wordlist.CreatedAt,
+			&wordlist.UpdatedBy,
+			&wordlist.UpdatedAt,
 		)
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrWordlistNotFound
@@ -140,4 +209,72 @@ func listWordlists(ctx context.Context, db *sql.DB, in *WordlistQuery) ([]*Wordl
 	}
 
 	return wordlists, nil
+}
+
+func getWordlist(ctx context.Context, db *sql.DB, in *WordlistQuery) (*Wordlist, error) {
+	wordlists, err := listWordlists(ctx, db, in)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(wordlists) == 0 {
+		return nil, ErrWordlistNotFound
+	}
+	return wordlists[0], nil
+}
+
+func saveWordlist(ctx context.Context, db *sql.DB, wordlist *Wordlist) error {
+	return database.WithTx(ctx, db, func(ctx context.Context, tx *sql.Tx) error {
+		updatedQuery, args := sq.Update("income_wordlist").
+			Set("word", wordlist.Word).
+			Set("category", wordlist.Category).
+			Set("updated_by", wordlist.UpdatedBy).
+			Set("updated_at", wordlist.UpdatedAt).
+			Where(sq.Eq{
+				"id": wordlist.ID,
+			}).
+			PlaceholderFormat(sq.AtP).
+			MustSql()
+
+		effected, err := tx.ExecContext(ctx, updatedQuery, args...)
+		if err != nil {
+			return fmt.Errorf("failed to update wordlist: %w", err)
+		}
+
+		rowsAffected, err := effected.RowsAffected()
+		if err != nil {
+			return fmt.Errorf("failed to get rows affected: %w", err)
+		}
+
+		if rowsAffected == 0 {
+			insertQuery, args := sq.Insert("income_wordlist").
+				Columns(
+					"word",
+					"category",
+					"created_by",
+					"created_at",
+					"updated_by",
+					"updated_at",
+				).
+				Values(
+					wordlist.Word,
+					wordlist.Category,
+					wordlist.CreatedBy,
+					wordlist.CreatedAt,
+					wordlist.UpdatedBy,
+					wordlist.UpdatedAt,
+				).
+				Suffix("SELECT SCOPE_IDENTITY()").
+				PlaceholderFormat(sq.AtP).
+				MustSql()
+
+			row := tx.QueryRowContext(ctx, insertQuery, args...)
+			if err := row.Scan(&wordlist.ID); err != nil {
+				return fmt.Errorf("failed to insert wordlist: %w", err)
+			}
+			return nil
+		}
+
+		return nil
+	})
 }
